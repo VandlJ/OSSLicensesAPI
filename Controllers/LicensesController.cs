c98a181a74636006303ae26cc3eeafe2fe3a00e7
VandlJ <jan.vandlicek@gmail.com>
2024-03-01 23:08:25 +0100

    Controllers update

--- Controllers/LicensesController.cs
+++ Controllers/LicensesController.cs
@@ -1,8 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using OSSApi.Data;
 using OSSApi.Models;
-using Microsoft.EntityFrameworkCore;
+
 namespace OSSApi.Controllers
 {
     [Route("api/[controller]")]
@@ -10,53 +15,94 @@ namespace OSSApi.Controllers
     public class LicensesController : ControllerBase
     {
         private readonly LicensesDbContext _context;
-        
+
         public LicensesController(LicensesDbContext context)
         {
             _context = context;
         }
-        
-        [HttpGet("All")]
-        public async Task<ActionResult<IEnumerable<Licenses>>> GetUsers()
-        {
-            var result = await _context.licenses.ToListAsync();
-            if (result.Count == 0)
-            {
-                return NotFound(new { Title = "Nothing found", Status = "404" });
-            }
-            return Ok(result);
-        }
-        
-        // GET: api/<LicensesController>
+
+        // GET: api/Licenses
         [HttpGet]
-        public IEnumerable<string> Get()
+        public async Task<ActionResult<IEnumerable<Licenses>>> Getlicenses()
         {
-            return new string[] { "value1", "value2" };
+            return await _context.licenses.ToListAsync();
         }
 
-        // GET api/<LicensesController>/5
+        // GET: api/Licenses/5
         [HttpGet("{id}")]
-        public string Get(int id)
+        public async Task<ActionResult<Licenses>> GetLicenses(int id)
         {
-            return "value";
+            var licenses = await _context.licenses.FindAsync(id);
+
+            if (licenses == null)
+            {
+                return NotFound();
+            }
+
+            return licenses;
         }
 
-        // POST api/<LicensesController>
-        [HttpPost]
-        public void Post([FromBody] string value)
-        {
-        }
-
-        // PUT api/<LicensesController>/5
+        // PUT: api/Licenses/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+        public async Task<IActionResult> PutLicenses(int id, Licenses licenses)
         {
+            if (id != licenses.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(licenses).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!LicensesExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
         }
 
-        // DELETE api/<LicensesController>/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
+        // POST: api/Licenses
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Licenses>> PostLicenses(Licenses licenses)
         {
+            _context.licenses.Add(licenses);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetLicenses", new { id = licenses.Id }, licenses);
+        }
+
+        // DELETE: api/Licenses/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteLicenses(int id)
+        {
+            var licenses = await _context.licenses.FindAsync(id);
+            if (licenses == null)
+            {
+                return NotFound();
+            }
+
+            _context.licenses.Remove(licenses);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool LicensesExists(int id)
+        {
+            return _context.licenses.Any(e => e.Id == id);
         }
     }
 }
--- obj/Debug/net8.0/OSSApi.AssemblyInfo.cs
+++ obj/Debug/net8.0/OSSApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OSSApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2ea118c2f6c822339180a59c74350d753039f24d")]
 [assembly: System.Reflection.AssemblyProductAttribute("OSSApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OSSApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
