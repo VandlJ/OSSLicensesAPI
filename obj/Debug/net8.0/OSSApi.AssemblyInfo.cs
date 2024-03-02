411cd2866c4b16c8ad3d7a459e87f63c115f0c3f
Jan Vandlíček <jan.vandlicek@gmail.com>
2024-03-03 00:03:46 +0100

    Testing

--- obj/Debug/net8.0/OSSApi.AssemblyInfo.cs
+++ obj/Debug/net8.0/OSSApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OSSApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7fb56000942c529eab74ddc2faeebecd44db1396")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6cbb1716a8674db5dbab8b2a5048768c5d34c8b1")]
 [assembly: System.Reflection.AssemblyProductAttribute("OSSApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OSSApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
