9c3343c5798d9a95766158a233a2b6cceb93706c
Jan Vandlíček <jan.vandlicek@gmail.com>
2024-03-21 10:25:21 +0100

    POST delete

--- Controllers/CompatibilityController.cs
+++ Controllers/CompatibilityController.cs
@@ -15,20 +15,6 @@ namespace OSSApi.Controllers
             _context = context;
         }
 
-        // POST: api/CompatibilityMatrix/CheckCompatibility
-        [HttpPost("CheckCompatibility")]
-        public async Task<ActionResult<string>> CheckCompatibility3(InLicenses license)
-        {
-            // Retrieve compatibility for the provided licenses from the database
-            var compatibility = await _context.license_compatibility
-                .Where(x => 
-                    (x.License1 == license.Name1 && x.License2 == license.Name2))
-                .Select(x => x.Compatibility)
-                .FirstOrDefaultAsync();
-
-            return compatibility ?? ""; // Return compatibility or an empty string if not found
-        }
-
         // GET: api/CompatibilityMatrix/CheckCompatibility
         [HttpGet("CheckCompatibility")]
         public async Task<ActionResult<CompatibilityResponseModel>> CheckCompatibility(string license1, string license2)
